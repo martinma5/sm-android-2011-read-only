683e35ea51d76a7d832ae6979313f3faaeae3c1c
Mike <martinma5@yahoo.com>
2011-01-30 19:02:23 -0500

    I don't know much about git

--- src/org/usjapan/schedule/ScheduleListActivity.java
+++ src/org/usjapan/schedule/ScheduleListActivity.java
@@ -1,7 +1,86 @@
 package org.usjapan.schedule;
 
 import android.app.ListActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.usjapan.sm.R;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 public class ScheduleListActivity extends ListActivity {
+	
+	private ArrayAdapter<Performace> aa;
+	private ArrayList<Performace> mPerformacelist = new ArrayList<Performace>();
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        getPerformaces();
+
+        aa = new ArrayAdapter<Performace>(this, android.R.layout.simple_list_item_1, mPerformacelist);        
+        setListAdapter(aa);
+
+    }
+    
+    void getPerformaces(){
+       InputStream is = this.getResources().openRawResource(R.raw.performance_schedule);
+       DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        try {
+			DocumentBuilder db = factory.newDocumentBuilder();
+			Document dom = db.parse(is);
+			Element docElement = dom.getDocumentElement();
+			
+			NodeList n1 = docElement.getElementsByTagName("performance");
+			if (n1 != null && n1.getLength() > 0)
+			{
+				for (int i = 0; i < n1.getLength(); i++ )
+				{      
+					Element permformace = (Element)n1.item(i);
+					Element name = (Element) permformace.getElementsByTagName("name").item(0);
+					Element start = (Element) permformace.getElementsByTagName("starttime").item(0);
+					Element end = (Element) permformace.getElementsByTagName("endtime").item(0);
+					Element desc = (Element) permformace.getElementsByTagName("description").item(0);
+					
+					String performacename = name.getFirstChild().getNodeValue();
+					String startime = start.getFirstChild().getNodeValue();
+					String endtime = end.getFirstChild().getNodeValue();
+					String descstring = desc.getFirstChild().getNodeValue(); 
+					
+					Performace performance = new Performace(performacename, startime, endtime, descstring);
+					addPerformace(performance);
+				}
+			}
+			
+			
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }; // end void getPerformaces
+
+    
+    private void addPerformace(Performace _aperformace)
+    {
+    	mPerformacelist.add(_aperformace);
+    }
 
 }
--- src/org/usjapan/sm/SmActivity.java
+++ src/org/usjapan/sm/SmActivity.java
@@ -1,7 +1,14 @@
 package org.usjapan.sm;
 
+import org.usjapan.schedule.ScheduleListActivity;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class SmActivity extends Activity {
     /** Called when the activity is first created. */
@@ -10,4 +17,30 @@ public class SmActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	super.onCreateOptionsMenu(menu);
+    	MenuInflater inflater = getMenuInflater();
+    	inflater.inflate(R.menu.sm_activity_menu, menu);
+		return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+		Toast.makeText(this, "hi", Toast.LENGTH_LONG);
+    	switch(item.getItemId()){
+    	case R.id.showvenders:
+    		//Do stuff here
+    	    return true;
+    	case R.id.showschedule:
+    	    Intent startschedule = new Intent(this,ScheduleListActivity.class);
+    		this.startActivity(startschedule);
+    	    return true;
+    	default:
+    		 return super.onOptionsItemSelected(item);
+    	}
+
+    }
+    
 }
